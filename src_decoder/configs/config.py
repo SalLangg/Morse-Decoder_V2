f10392cd8be0d30af45da0606fdf3a457cd22ab9
DavidM <medvedev03dv@gmail.com>
2025-06-24 10:36:35 +0500

    MLE project_start

---
+++ src_decoder/configs/config.py
@@ -0,0 +1,60 @@
+from pydantic import BaseModel, PositiveInt, PositiveFloat
+from typing import Literal
+from pathlib import Path
+import yaml
+
+class DataConfig(BaseModel):
+    sample_rate: PositiveInt = 8000
+    n_mels: PositiveInt = 128
+    n_fft: PositiveInt = 400
+    hop_length: PositiveInt = 180
+    top_db: PositiveInt = 80
+    freq_mask: PositiveInt = 15
+    time_mask: PositiveInt = 20
+    blank_char: str = "_"
+    audio_dir: str = "morse_dataset/morse_dataset"
+    train_csv: str = "morse_dataset/train.csv"
+    test_csv: str = "morse_dataset/test.csv"
+    val_size: PositiveFloat = 0.15
+
+class TrainingConfig(BaseModel):
+    seed: PositiveInt = 42
+    batch_size: PositiveInt = 64
+    epochs: PositiveInt = 70
+    learning_rate: PositiveFloat = 0.002
+    weight_decay: PositiveFloat = 0.00001
+    early_stopping_patience: PositiveInt = 5
+    lr_patience: PositiveInt = 3
+    lr_factor: PositiveFloat = 0.5 
+
+class ModelConfig(BaseModel):
+    first_fe_count: PositiveInt = 16
+    second_fe_count: PositiveInt = 32
+    third_fe_count: PositiveInt = 32
+    quad_fe_count: PositiveInt = 32
+    padding: Literal["same", "valid"] = "same"
+    maxpool_kernel: PositiveInt = 2
+    kernel_size: PositiveInt = 3
+    neuron_count: PositiveInt = 128
+    gru_hidden: PositiveInt = 256
+    dropout: PositiveFloat = 0.3
+
+class Config(BaseModel):
+    data: DataConfig
+    training: TrainingConfig
+    model: ModelConfig
+
+
+def load_config(config_path: str = "configs/config.yaml", base=False) -> Config:
+    """Load config
+    
+    Parameters: 
+        config_path: srt -> 
+        base: bool -> use/unuse yaml values
+    """
+    if base:
+        return Config()
+    else:
+        with open(Path(__file__).parent / config_path) as f:
+            raw_config = yaml.safe_load(f)
+        return Config(**raw_config)
